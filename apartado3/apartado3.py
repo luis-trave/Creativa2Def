a58ca5c7b23128ade0614f904558f8055e403e83
luis-trave <106604808+luis-trave@users.noreply.github.com>
2023-02-02 16:44:08 +0100

    Update and rename bloque3.py to apartado3.py

--- apartado3/apartado3.py
+++ apartado3/apartado3.py
@@ -2,6 +2,12 @@
 from subprocess import call
 import os
 
+os.system('sudo docker rm /36-productpage')
+os.system('sudo docker rm /36-details')
+os.system('sudo docker rm /36-reviews')
+os.system('sudo docker rm /36-ratings')
+
+
 os.system('git clone https://github.com/CDPS-ETSIT/practica_creativa2.git')
 os.chdir('practica_creativa2/bookinfo/src/reviews')
 os.system('sudo docker run --rm -u root -v "$(pwd)":/home/gradle/project -w /home/gradle/project gradle:4.8.1 gradle clean build')
