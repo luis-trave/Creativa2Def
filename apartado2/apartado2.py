34e60c17a010f93397986adb550ace31aec65ed4
luis-trave <106604808+luis-trave@users.noreply.github.com>
2023-02-02 13:53:48 +0100

    Update apartado2.py

--- apartado2/apartado2.py
+++ apartado2/apartado2.py
@@ -2,5 +2,9 @@
 from subprocess import call
 import os
 
+
+os.system('sudo apt-get remove docker docker-engine docker.io containerd runc')
+os.system('sudo apt-get install docker.io')
+
 call(['sudo','docker', 'build', '-t', '36/product-page', '.'])
 os.system('sudo docker run -it --name 36-productpage -p 9080:9080 --env GROUP_NUMBER=36 36/product-page')
