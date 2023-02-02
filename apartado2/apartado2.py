c1ffa64764283e0dd460e255d9c0e77936769b2e
luis-trave <106604808+luis-trave@users.noreply.github.com>
2023-02-02 13:58:07 +0100

    Update apartado2.py

--- apartado2/apartado2.py
+++ apartado2/apartado2.py
@@ -6,5 +6,6 @@ import os
 os.system('sudo apt-get remove docker docker-engine docker.io containerd runc')
 os.system('sudo apt-get install docker.io')
 
+os.system('docker rmi $(docker imagenes -q)')
 call(['sudo','docker', 'build', '-t', '36/product-page', '.'])
 os.system('sudo docker run -it --name 36-productpage -p 9080:9080 --env GROUP_NUMBER=36 36/product-page')
