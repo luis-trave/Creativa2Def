474dd1fcaab104e3bead81e95772d33edeb7f059
luis-trave <106604808+luis-trave@users.noreply.github.com>
2023-02-02 14:16:46 +0100

    Update apartado2.py

--- apartado2/apartado2.py
+++ apartado2/apartado2.py
@@ -2,6 +2,6 @@
 from subprocess import call
 import os
 
-os.system('sudo docker rmi -f $(sudo docker images -q)')
+os.system('sudo docker rm /36-productpage')
 call(['sudo','docker', 'build', '-t', '36/product-page', '.'])
 os.system('sudo docker run -it --name 36-productpage -p 9080:9080 --env GROUP_NUMBER=36 36/product-page')
